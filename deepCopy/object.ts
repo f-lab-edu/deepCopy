b0ae499da9f3fbd042edf0f176d5ea44d84ec4b8
ainochi-kor <kthrkdals@gmail.com>
2023-07-16 22:14:58 +0900

    add: check target?.hasOwnProperty

--- deepCopy/object.ts
+++ deepCopy/object.ts
@@ -9,7 +9,7 @@ export const deepCopyObject = <T>(target: T, deepCopy: (value: any) => T) => {
   //* Object.getPrototypeOf() 메서드는 지정된 객체의 프로토타입(가령 내부 [[Prototype]] 속성값)을 반환합니다.
   const clone = Object.create(Object.getPrototypeOf(target));
   for (let key in target) {
-    if (target.hasOwnProperty(key)) {
+    if (target?.hasOwnProperty && target.hasOwnProperty(key)) {
       clone[key] = deepCopy(target[key]);
     }
   }
